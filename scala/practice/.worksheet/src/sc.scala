49ae97670ecfc0d36a75efb2413e058588b01003
ghk829 <ghk829@gmail.com>
2017-02-23 16:45:27 +0900

    list practice

---
+++ scala/practice/.worksheet/src/sc.scala
@@ -0,0 +1,4 @@
+import java.io.File
+object sc {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(50); val res$0 = 
+  List(1)::List(2);System.out.println("""res0: List[Any] = """ + $show(res$0))}
+}
